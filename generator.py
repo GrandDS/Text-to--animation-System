aeac8d14b0c95e21d9a5bd372b21b57876e9c9ec
Tomi Kolawole <148702700+GrandDS@users.noreply.github.com>
2026-08-31 09:45:51 +0100

    Remove legacy generator.py

--- generator.py
+++
@@ -1,5 +0,0 @@
-class ImageGenerator:
-    def generate(self, prompt):
-        filename = "generated_output.png"
-        message = f"Generated mock image for prompt: {prompt}"
-        return {"filename": filename, "message": message}
